fcdc36b0c73b47c2a113e5b674a473aa3ba715ad
shalini2005 <104878283+shalini2005@users.noreply.github.com>
2022-05-04 14:33:00 +1200

    Add files via upload

---
+++ 00_tinker_template.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import *
+
+class Convertor:
+    def __init__(self, parent):
+        print("hello world")
+        
+#main routine
+
+if __name__== "__main__":
+    root= Tk()
+    root.title("Temperature Convertor")
+    something = Convertor(root)
+    root.mainloop()
+        
+    
